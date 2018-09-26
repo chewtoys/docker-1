24e35fbbac83eeeb26bd5b143307af72aa48b71a
Rodrigo Carvalho <rdgacarvalho@gmail.com>
2018-09-26 17:51:19 +0200

    shell script to unseal vault

--- vault/config/unseal.sh
+++ vault/config/unseal.sh
@@ -7,20 +7,40 @@ UNSEAL_KEYS=ukeys.txt
 # Initializing vault
 vault operator init > $KEYS_FILE
 
+function init() {
+
 if [ $? -eq 0 ]; then
     echo "Vault initialized!"
 else
     echo "Vault initialized already. Wait..."
 fi
 
-# Unseling Vault
-KEYS_TOKEN=$(cat $KEYS_FILE | grep "Unseal" | awk '{print $4}' > $UNSEAL_KEYS)
-UNSEAL_KEYS=ukeys.txt
+    unseal
 
-while read lines
- do 
-    vault operator unseal $lines
-done < $UNSEAL_KEYS
+}
 
-# List vault status
-vault status
+function unseal() {
+    # Unseling Vault
+    KEYS_TOKEN=$(cat $KEYS_FILE | grep "Unseal" | awk '{print $4}' > $UNSEAL_KEYS)
+    UNSEAL_KEYS=ukeys.txt
+
+    while read lines
+    do 
+        echo "Unsealing ... $lines"
+        vault operator unseal $lines
+    done < $UNSEAL_KEYS
+
+    secrets
+}
+
+function secrets(){
+    
+    ROOT_TOKEN=$(cat $KEYS_FILE | grep "Token" | awk '{print $4}')
+    # Root token
+    echo "Root Token:: $ROOT_TOKEN"
+    
+}
+
+
+
+$@
