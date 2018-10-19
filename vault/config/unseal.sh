73878a4a50eee01186799528a76fa172c94dfd87
Rodrigo Carvalho <rdgacarvalho@gmail.com>
2018-10-19 15:28:24 +0200

    added some excluded files

--- vault/config/unseal.sh
+++ vault/config/unseal.sh
@@ -33,10 +33,58 @@ function unseal() {
         vault operator unseal $lines
     done < $UNSEAL_KEYS
 
-    secrets
+    audit
 }
 
-function secrets(){
+function audit() {
+    
+    ROOT_TOKEN=$(cat $KEYS_FILE | grep "Token" | awk '{print $4}')
+    # Root token
+    echo "Root Token:: $ROOT_TOKEN"
+
+    # Login into Vault 
+    vault login $ROOT_TOKEN > /dev/null
+
+    mkdir -p /var/logs/vault/
+
+    vault audit enable file file_path=/var/logs/vault/audit.log
+
+    okta
+}
+
+function okta() {
+    ROOT_TOKEN=$(cat $KEYS_FILE | grep "Token" | awk '{print $4}')
+    # Root token
+    echo "Root Token:: $ROOT_TOKEN"
+
+    # Login into Vault 
+    vault login $ROOT_TOKEN > /dev/null
+    
+    vault auth enable okta
+
+    vault write auth/okta/config base_url="okta-emea.com" organization="Okta-IT-Operations" token="00Xape2-53TMT4z2siosZXhR5UL-yDSS_V9cPqYIdR"
+
+    vault write auth/okta/groups/Okta-IT-Operations policies=Operations
+
+    database
+
+}
+
+function database() {
+
+    ROOT_TOKEN=$(cat $KEYS_FILE | grep "Token" | awk '{print $4}')
+    # Root token
+    echo "Root Token:: $ROOT_TOKEN"
+
+    # Login into Vault 
+    vault login $ROOT_TOKEN > /dev/null
+
+    vault secrets enable database
+
+}
+
+
+function secrets() {
     
     ROOT_TOKEN=$(cat $KEYS_FILE | grep "Token" | awk '{print $4}')
     # Root token
@@ -47,7 +95,7 @@ function secrets(){
     
     # Activating SSH
     vault secrets enable ssh
-    vault write ssh/roles/no_ssh_key key_type=otp default_user=vagrant allowed_users=root,admin port=22 cidr_list=0.0.0.0/0
+    vault write ssh/roles/no_ssh_key key_type=otp default_user=ubuntu allowed_users=root,vogon,admin port=22 cidr_list=0.0.0.0/0
     vault write ssh/creds/no_ssh_key ip=0.0.0.0
 
     vault secrets enable -path=ssh-client ssh
@@ -56,7 +104,7 @@ function secrets(){
 
 # Keys file name
 
-KEYS_FILE=keys.txt
-UNSEAL_KEYS=ukeys.txt
+KEYS_FILE=keys.txt 2> /dev/null
+UNSEAL_KEYS=ukeys.txt 2> /dev/null
 
 $@
